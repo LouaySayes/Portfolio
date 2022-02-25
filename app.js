8350c39019be6cc972e6f560e58c3f3a4c08db68
Louay Sayes <96046268+LouaySayes@users.noreply.github.com>
2022-02-25 11:23:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+       
+       $('.btn-navigation').click(function(){
+        $(this).find('.barre').toggleClass('white');
+        $('.navigation').toggleClass('isopen');
+       })
+});
